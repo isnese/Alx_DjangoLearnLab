92a43890c09a01dc40f16856b1c5b8bde578d13a
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-08-18 18:32:56 +0300

    Add files via upload

--- django-models/bookshelf/models.py
+++ django-models/bookshelf/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(default=2000)
 
-    def __str__(self):
-        return self.title
--- django-models/bookshelf/views.py
+++ django-models/bookshelf/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from .models import Book
+def index(request):
+    return HttpResponse(f"{Book.objects.all()}")
