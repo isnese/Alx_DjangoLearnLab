663951e7304c4e0582ad4349a036fe7d30de1e5f
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-08-18 17:53:19 +0300

    Add files via upload

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,49 +1,35 @@
-import os
 import django
+import os
+from django.conf import settings
+from relationship_app.models import Author, Book, Library, Librarian
 
+# Setup Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-models.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library, Librarian
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    for book in books:
+        print(f'Book Title: {book.title}')
 
-# Query all books by a specific author
-def books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)
-        for book in books:
-            print(book.title)
-    except Author.DoesNotExist:
-        print(f'Author "{author_name}" does not exist.')
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    for book in books:
+        print(f'Book Title: {book.title}')
 
-# List all books in a library
-def books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        books = library.books.all()
-        for book in books:
-            print(book.title)
-    except Library.DoesNotExist:
-        print(f'Library "{library_name}" does not exist.')
+def retrieve_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    print(f'Librarian Name: {librarian.name}')
 
-# Retrieve the librarian for a library
-def librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = Librarian.objects.get(library=library)
-        print(librarian.name)
-    except Library.DoesNotExist:
-        print(f'Library "{library_name}" does not exist.')
-    except Librarian.DoesNotExist:
-        print(f'Librarian for library "{library_name}" does not exist.')
-
-# Example usage
-if __name__ == '__main__':
-    print("Books by Author:")
-    books_by_author('J.K. Rowling')
+if __name__ == "__main__":
+    print("Books by Author 'J.K. Rowling':")
+    query_books_by_author('J.K. Rowling')
     
-    print("\nBooks in Library:")
-    books_in_library('Central Library')
-    
-    print("\nLibrarian for Library:")
-    librarian_for_library('Central Library')
+    print("\nBooks in Library 'Central Library':")
+    list_books_in_library('Central Library')
+
+    print("\nLibrarian for Library 'Central Library':")
+    retrieve_librarian_for_library('Central Library')
