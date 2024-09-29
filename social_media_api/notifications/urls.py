134aeff563f7c870ff3e4e920a048cb955161ca9
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-09-29 17:04:02 +0300

    Delete social_media_api directory

--- social_media_api/notifications/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import NotificationListView
-
-urlpatterns = [
-    path('notifications/', NotificationListView.as_view(), name='notifications'),
-]
